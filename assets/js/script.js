d21f5403086d4516a8e22abd95d38b72e5f74caf
Michael Bashford <mikebashford@gmail.com>
2021-12-11 14:01:35 -0600

    Added the dates to the page and the 5 day forecast cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 const API_KEY = '151725f4087f7ff463e513a01d683d1f';
 
 var searchCities = JSON.parse(localStorage.getItem('citiesArray')) || [];
+var numberOfCards = 5;
 
 var saveSearches = (cityName) =>
 {
   searchCities.push(cityName);
   localStorage.setItem('citiesArray', JSON.stringify(searchCities));
-  
+
   if(searchCities.length <= 1)
   {
     var dividerEl = document.createElement('hr');
@@ -18,6 +19,8 @@ var saveSearches = (cityName) =>
   $(buttonEl).addClass('saved-city');
   buttonEl.innerHTML = cityName;
   $('.search-bar').append(buttonEl);
+
+
 }
 
 $('.search-btn').on('click', function()
@@ -38,18 +41,47 @@ var getWeather = (city) =>
     .then((response) => response.json())
     .then((data) => 
     {
-      $('.city').text(city);
-      var {temp, humidity, wind_speed, uvi} = data.current;
+      
+      var {temp, humidity, wind_speed, uvi, dt} = data.current;
       var {icon} = data.current.weather[0];
 
+      var date = new Date(dt * 1000).toLocaleDateString();
+
+      $('.city').text(city + " " + date);
       $('.temp').text('Temp: ' + temp + ' °F');
       $('.wind').text('Wind: ' + wind_speed + ' MPH');
       $('.humidity').text('Humidity: ' + humidity + ' %');
       $('.uvi').text("UV index: " + uvi);
-      $('.weather-icon').attr('src', 'https://openweathermap.org/img/wn/' + icon + '@2x.png');
-    });
-  });
+      $('.weather-icon').attr('src', 'https://openweathermap.org/img/wn/' + icon + '.png');
+
+      for(i = 0; i < numberOfCards; i++)
+      {
+        var dateEl = document.createElement('p');
+        var iconEl = document.createElement('img');
+        var tempEl = document.createElement('p');
+        var windEl = document.createElement('p');
+        var humidEl = document.createElement('p');
+        var {max} = data.daily[i].temp; 
+        var {wind_speed, humidity} = data.daily[i];
+        var {dt} = data.daily[i+1];
+        var {icon} = data.daily[i].weather[0];
+
+        var date = new Date(dt * 1000).toLocaleDateString();
+
+        console.log(date, max, wind_speed, humidity, icon);
+
+        $(dateEl).text(date);
+        $(tempEl).text('Temp: ' + max + ' °F');
+        $(windEl).text('Wind: ' + wind_speed + ' MPH');
+        $(humidEl).text('Humidity: ' + humidity + ' %');
+        $(iconEl).attr('src', 'https://openweathermap.org/img/wn/' + icon + '.png');
+
+        $('.card' + i).append(dateEl, iconEl, tempEl, windEl, humidEl);
+      }
+    })
+  })
 }
+
 
 
 
