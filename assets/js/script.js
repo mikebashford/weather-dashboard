e68127ed7f997253873b61be719339f502720f75
Michael Bashford <mikebashford@gmail.com>
2021-12-11 17:19:54 -0600

    Stylized the page more, added check for buttons being added under the search button.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,35 +1,21 @@
+//API key string so I don't have to type it twice
 const API_KEY = '151725f4087f7ff463e513a01d683d1f';
-
-var searchCities = JSON.parse(localStorage.getItem('citiesArray')) || [];
+//How many forecast cards I want to create
 var numberOfCards = 5;
+var savedForecast = {};
 
-var saveSearches = (cityName) =>
-{
-  searchCities.push(cityName);
-  localStorage.setItem('citiesArray', JSON.stringify(searchCities));
-
-  if(searchCities.length <= 1)
-  {
-    var dividerEl = document.createElement('hr');
-    $(dividerEl).addClass('rounded');
-    $('.search-bar').append(dividerEl);
-  }
-
-  var buttonEl = document.createElement('button');
-  $(buttonEl).addClass('saved-city');
-  buttonEl.innerHTML = cityName;
-  $('.search-bar').append(buttonEl);
-
-
-}
-
+//Button that allows us find the weather when a city is entered
 $('.search-btn').on('click', function()
 {
+  clearPreviousData();
   var city = $('#city').val();
-  saveSearches(city);
-  getWeather(city);
+  if( city !== null && city !== "" && city !== " ")
+  {
+    $('#city').val(" ");
+    getWeather(city);
+  }
 })
-
+//Gets the weather by city, converts to lat/lon, gets the needed forecast items
 var getWeather = (city) =>
 {
   fetch('https://api.openweathermap.org/data/2.5/forecast?q='+ city + '&units=imperial&appid=' + API_KEY)
@@ -44,15 +30,16 @@ var getWeather = (city) =>
       
       var {temp, humidity, wind_speed, uvi, dt} = data.current;
       var {icon} = data.current.weather[0];
-
       var date = new Date(dt * 1000).toLocaleDateString();
+      var weatherIconEl = document.createElement('img');
 
+      $(weatherIconEl).attr('src', 'https://openweathermap.org/img/wn/' + icon + '.png');
       $('.city').text(city + " " + date);
+      $('.icon-column').append(weatherIconEl);
       $('.temp').text('Temp: ' + temp + ' °F');
       $('.wind').text('Wind: ' + wind_speed + ' MPH');
       $('.humidity').text('Humidity: ' + humidity + ' %');
-      $('.uvi').text("UV index: " + uvi);
-      $('.weather-icon').attr('src', 'https://openweathermap.org/img/wn/' + icon + '.png');
+      $('.uvi').text("UV index: " + uvi); 
 
       for(i = 0; i < numberOfCards; i++)
       {
@@ -61,28 +48,77 @@ var getWeather = (city) =>
         var tempEl = document.createElement('p');
         var windEl = document.createElement('p');
         var humidEl = document.createElement('p');
-        var {max} = data.daily[i].temp; 
-        var {wind_speed, humidity} = data.daily[i];
-        var {dt} = data.daily[i+1];
-        var {icon} = data.daily[i].weather[0];
-
+        var {max} = data.daily[i+1].temp; 
+        var {wind_speed, humidity, dt} = data.daily[i+1];
+        var {icon} = data.daily[i+1].weather[0];
         var date = new Date(dt * 1000).toLocaleDateString();
-
-        console.log(date, max, wind_speed, humidity, icon);
 
         $(dateEl).text(date);
         $(tempEl).text('Temp: ' + max + ' °F');
         $(windEl).text('Wind: ' + wind_speed + ' MPH');
         $(humidEl).text('Humidity: ' + humidity + ' %');
         $(iconEl).attr('src', 'https://openweathermap.org/img/wn/' + icon + '.png');
-
         $('.card' + i).append(dateEl, iconEl, tempEl, windEl, humidEl);
+        $('.card' + i).attr('style', 'visibility:visible;')
       }
+      saveSearches(city);
     })
   })
 }
 
+//Saves all the information from the previous search
+var saveSearches = (cityName) =>
+{
 
+  savedForecast =
+  {
+    weatherInfo: $('.weather-info'),
+    forecastCard: $('.forecast')
+  }
 
+  var saveHTML =
+  {
+    weather: savedForecast.weatherInfo[0].outerHTML,
+    forecast: savedForecast.forecastCard[0].outerHTML
+  }
 
+  localStorage.setItem(JSON.stringify(cityName), JSON.stringify(saveHTML));
 
+  if(localStorage.length <= 1)
+  {
+    var dividerEl = document.createElement('hr');
+    $(dividerEl).addClass('rounded');
+    $('.search-bar').append(dividerEl);
+  }
+  var buttonEl = document.createElement('button');
+  $(buttonEl).addClass('saved-city');
+  buttonEl.innerHTML = cityName;
+  $('.search-bar').append(buttonEl);
+
+  $(buttonEl).on('click', function()
+  {
+    console.log('I was pressed.')
+    getPreviousSearch(cityName);
+  })
+}
+
+//Clears the data from the previous search
+var clearPreviousData = () =>
+{
+  $('.row').empty();
+  for(i = 0; i < numberOfCards; i++)
+  {
+    var colEl = document.createElement('div');
+    $(colEl).addClass('column');
+    var cardEl = document.createElement('div');
+    $(cardEl).addClass('card'+ i);
+    $(colEl).append(cardEl);
+    $('.row').append(colEl);
+  }
+  $('.icon-column').empty();
+}
+
+var getPreviousSearch = (cityName) =>
+{
+  localStorage.getItem(cityName);
+}
